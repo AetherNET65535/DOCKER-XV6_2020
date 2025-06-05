d9af14372486f52744af05b12b86925384a02f3a
AetherNET65535 <aethernet65535@gmail.com>
2025-06-05 21:04:34 +0800

    refactor: sleep update

--- user/sleep.c
+++ user/sleep.c
@@ -4,25 +4,24 @@
 
 int main (int argc, char *argv[])
 {
-    // CHECK: is user enter argument same with 2?
+    // if the argument is not 2 (sleep <ticks>)
     if (argc != 2)
     {
-        fprintf (2, "USAGE: sleep <ticks>\n");
+        fprintf (2, "usage: sleep <ticks>\n");
         exit(1);
     }
     
-    // CHANGE: TO INTERGER, AND TO SECONDS
+    // change ascii to integer
     int ticks = atoi(argv[1]);
-    int adjusted_ticks = ticks * 10;
 
-    // CHECK: if ticks smaller than 0, set it to 0
-    if (adjusted_ticks < 0)
+    // turn back time: disabled
+    if (ticks < 0)
     {
-        adjusted_ticks = 0;
+        fprintf (2, "ticks must be a positive integer\n");
+        exit(1);
     }
 
-    // CALL: sleep (thank you xv6, this make this code ez 5000%)
-    sleep(adjusted_ticks);
+    sleep(ticks);
 
     exit(0);
 }
