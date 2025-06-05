09cf58f132f739a237d00c1dd0a3f1ee4891844d
AetherNET65535 <aethernet65535@gmail.com>
2025-06-05 23:23:36 +0800

    refactor: update pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -7,27 +7,37 @@
 
 int main (int argc, char *argv[])
 {
-    // CHECK: is user enter argument samw with 1?
     if (argc != 1)
     {
-        fprintf(2, "USAGE: pingpong\n");
+        fprintf(2, "usage: pingpong\n");
         exit(1);
     }
 
-    int pid, p2c[2], c2p[2];
+    int pid;
+
+    // one-way pipes for communication
+    // p2c: parent to child
+    // c2p: child to parent
+    int p2c[2], c2p[2]; 
     char signal = 0;
     
-    pipe(p2c);
+    // create pipes
+    pipe(p2c); 
     pipe(c2p);
 
-    if ((pid = fork()) > 0)
+    pid = fork();
+
+    if (pid > 0) // parent process
     {
+        // parent process just needs to write[p2c] and read[c2p]
         close(p2c[READ]);
         close(c2p[WRITE]);
 
+        // tee off: send a byte signal to child
         write(p2c[WRITE], &signal, 1);
         close(p2c[WRITE]);
 
+        // waiting for catch: blocking until child sends a byte signal
         read(c2p[READ], &signal, 1);
         close(c2p[READ]);
 
@@ -35,16 +45,19 @@ int main (int argc, char *argv[])
         
         exit(0);
     }
-    else if (pid == 0)
+    else if (pid == 0) // child process
     {
+        // child process just needs to read[p2c] and write[c2p]
         close(c2p[READ]);
         close(p2c[WRITE]);
 
+        // waiting for catch: blocking until parent sends a byte signal
         read(p2c[READ], &signal, 1);
         close(p2c[READ]);
 
         printf("%d: Received Ping\n", getpid());
         
+        // tee off: send a byte signal to parent
         write(c2p[WRITE], &signal, 1);
         close(c2p[WRITE]);
 
@@ -56,4 +69,3 @@ int main (int argc, char *argv[])
         exit(1);
     }
 }
-
